d62a94bf63394b224f58a89358d82eaf52e50bcf
vn02063007 <bui.minh.tuan@hotmail.com>
2022-10-17 19:06:37 +0800

    added

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,21 +1,46 @@
-from email import header
-from itertools import count
 import os
 import csv
 
-csv_path = os.path.join('..','Resources','budget_data.csv')
+
+csv_path = os.path.join('Resources','budget_data.csv')
 
 #List to store data
-Total_no_months = []
-Net_total_profit_lost = []
+Total_months = 0
+Total = []
 Change =[]
 Greatest_Increse = []
 Greatest_Decrease = []
+total_net = 0
 
+#def
+def sum(data):
+    date = str(data[0])
+    profit = int(data[1])
+
+
+#read csv file
 with open(csv_path) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     header = next(csv_reader)
 
-Total_no_months = count(csv_reader)
+    first_row = next(csv_reader)
+    Total_months += 1
+    total_net += int(first_row[1])
+    prev_net = int(first_row[1])
 
-print(Total_no_months)
+    for row in csv_reader:
+
+# Total number of months
+        Total_months += 1
+#The net total amount of profit & losses
+        total_net += int(row[1])
+
+
+output = (
+        f"Financial Analysis\n"
+        f"----------------------------\n"
+        f"Total Months: {Total_months}\n"
+        f"Total: $ {total_net}\n"  
+
+)
+print (output)
