ef43040ffe8bd37832d8f231b53e0d7c8a002de5
vn02063007 <bui.minh.tuan@hotmail.com>
2022-10-17 19:23:28 +0800

    added

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,3 +1,4 @@
+from distutils.text_file import TextFile
 import os
 import csv
 
@@ -11,6 +12,8 @@ Change =[]
 Greatest_Increse = []
 Greatest_Decrease = []
 total_net = 0
+net_change_list = []
+month_of_change = []
 
 #def
 def sum(data):
@@ -34,13 +37,37 @@ with open(csv_path) as csv_file:
         Total_months += 1
 #The net total amount of profit & losses
         total_net += int(row[1])
+#Track the net change
+        net_change = int(row[1])- prev_net
+        prev_net = int(row[1])
+        net_change_list += [net_change]
+        month_of_change += [row[0]]
+#Calculate the greatest increase
+if net_change > Greatest_Increse[1]:
+        Greatest_Increse[0] = row[0]
+        Greatest_Increse[1] = net_change
+#Calculate the greatest decrease
+if net_change < Greatest_Decrease[1]:
+        Greatest_Decrease[0] = row[0]
+        Greatest_Decrease[1] = net_change
 
+
+#Average net change
+net_monthly_avg= sum(net_change_list)/ len(net_change_list)
 
 output = (
         f"Financial Analysis\n"
         f"----------------------------\n"
         f"Total Months: {Total_months}\n"
         f"Total: $ {total_net}\n"  
-
+        f"Average Change: $ {net_monthly_avg:.2f}\n"
+        f"Greatest Increase in Profits: {Greatest_Increse[0]} (${Greatest_Increse[1]})\n"
+        f"Greatest Decrease in Profits: {Greatest_Decrease[0]} (${Greatest_Decrease[1]})\n"
 )
+
+#Print output
 print (output)
+
+#Export
+with open('analysis',"w") as txt_file:
+        TextFile.write('Analysis')
